6719dd0b4ddd9ba00dc500700d694e0aa380e866
Max Speck <maxspeck@Maxs-MBP.fritz.box>
2025-01-17 22:47:59 +0100

    readme

--- components/login.py
+++ components/login.py
@@ -2,14 +2,18 @@ import streamlit as st
 
 
 def login():
-    """Simple login page."""
     st.title("Login")
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
     login_button = st.button("Login")
 
+    # Access credentials from secrets
+    ADMIN_USERNAME = st.secrets["credentials"]["username"]
+    ADMIN_PASSWORD = st.secrets["credentials"]["password"]
+
     if login_button:
-        if username == "admin" and password == "password":
+        if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
             st.session_state["logged_in"] = True
+            st.success("Successfully logged in!")
         else:
             st.error("Invalid username or password")
